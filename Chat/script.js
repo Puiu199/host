fb0e6d5796d027e8f0acd54d43b6994e6310e99e
Puiu Dumitru99 <puiudumitru99@gmail.com>
2024-11-27 11:06:00 +0200

    Update code at chat

--- Chat/script.js
+++ Chat/script.js
@@ -7,25 +7,48 @@ let conversation = [];
 function addConversation(array) {
   for (let i = 0; i < 1; i++) {
     const element = array[i];
-    console.log(array);
+    // console.log(array);
     const paragraf = document.createElement("p");
-    console.log(element.Sende);
+    // console.log(element.Sende);
     paragraf.classList.add(element.Sende);
     paragraf.textContent = element.Mesage;
     ChatI.appendChild(paragraf);
-    Chatf.appendChild(paragraf.cloneNode(true));
+    // Chatf.appendChild(paragraf.cloneNode(true));
   }
 }
+let click = 0;
 
-document.querySelector("#sendI").addEventListener("click", function () {
+function sentMesage() {
   conversation.unshift({ Sende: "IAM", Mesage: mesage.value });
-  console.log(conversation[0]);
+  // console.log(conversation[0]);
   mesage.value = " ";
   addConversation(conversation);
-});
-document.querySelector("#sendF").addEventListener("click", function () {
-  conversation.unshift({ Sende: "friend", Mesage: messgeF.value });
-  console.log(conversation[0]);
-  messgeF.value = " ";
+}
+
+function receive() {
+  conversation.unshift({ Sende: "friend", Mesage: " Raspunsul prietenului " });
+  // console.log(conversation[0]);
+  mesage.value = " ";
   addConversation(conversation);
+}
+
+// document.querySelector("#sendF").addEventListener("click", function () {
+//   conversation.unshift({ Sende: "friend", Mesage: messgeF.value });
+//   console.log(conversation[0]);
+//   messgeF.value = " ";
+//   addConversation(conversation);
+// });
+
+document.querySelector("#sendI").addEventListener("click", function (event) {
+  click++;
+  if (click == 1) {
+    setTimeout(() => {
+      if (click === 1) {
+        sentMesage();
+      } else {
+        receive();
+      }
+      click = 0;
+    }, 220);
+  }
 });
