de6c262aa5a9c0afe97c5ce6cf3da4eacc3c2264
Puiu Dumitru99 <puiudumitru99@gmail.com>
2024-12-02 19:12:52 +0200

    first commit

---
+++ 02. Number-Convertor/script.js
@@ -0,0 +1,18 @@
+//trebuie sa convertesti numarul introdus in dependenta de ce a ales clientu(Binar sau hexa decimal)
+// cand dai click
+// pe butonul convert it in inputul result sa apara rezultatul convertit
+let number = document.querySelector("#input");
+let selectMenuTo = document.querySelector("#selectMenuTo");
+
+option = document.createElement("option");
+
+option.document.createTextNode("sajdaskdhsklj");
+
+const butonul = document.querySelector("#container button");
+
+butonul.addEventListener("click", function () {
+  console.log(option);
+
+  selectMenuTo.cloneNode(option, true);
+  console.log(number.value);
+});
